2750e63e5647f7bfc350e128d5e56c6529a1be30
Xaavier <xavier.munoz.pisco@gmail.com>
2020-08-06 23:09:58 -0500

    Actividad

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.ImageCapture;
 import androidx.core.app.ActivityCompat;
+import androidx.documentfile.provider.DocumentFile;
 
 import android.Manifest;
 import android.app.DownloadManager;
@@ -19,13 +20,16 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int RQS_OPEN_DOCUMENT_TREE = 2;
     ImageView imageView;
+    TextView textInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,48 +41,58 @@ public class MainActivity extends AppCompatActivity {
         permisos.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         permisos.add(Manifest.permission.READ_EXTERNAL_STORAGE);
         permisos.add(Manifest.permission.WRITE_CALENDAR);
-        imageView=(ImageView) findViewById(R.id.imageView);
-
-
+        imageView = (ImageView) findViewById(R.id.imageView);
+        textInfo = (TextView) findViewById(R.id.info);
         getPermission(permisos);
     }
 
-    public void getPermission(ArrayList<String> permisosSolicitados){
+    public void BuscarLugar() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        startActivityForResult(intent, RQS_OPEN_DOCUMENT_TREE);
+    }
+
+    public void getPermission(ArrayList<String> permisosSolicitados) {
 
         ArrayList<String> listPermisosNOAprob = getPermisosNoAprobados(permisosSolicitados);
-        if (listPermisosNOAprob.size()>0)
-             if (Build.VERSION.SDK_INT >= 23)
-                  requestPermissions(listPermisosNOAprob.toArray(new String[listPermisosNOAprob.size()]), 1);
+        if (listPermisosNOAprob.size() > 0)
+            if (Build.VERSION.SDK_INT >= 23)
+                requestPermissions(listPermisosNOAprob.toArray(new String[listPermisosNOAprob.size()]), 1);
 
     }
 
 
-    public ArrayList<String> getPermisosNoAprobados(ArrayList<String>  listaPermisos) {
+    public ArrayList<String> getPermisosNoAprobados(ArrayList<String> listaPermisos) {
         ArrayList<String> list = new ArrayList<String>();
-        for(String permiso: listaPermisos) {
+        for (String permiso : listaPermisos) {
             if (Build.VERSION.SDK_INT >= 23)
-                 if(checkSelfPermission(permiso) != PackageManager.PERMISSION_GRANTED)
-                     list.add(permiso);
+                if (checkSelfPermission(permiso) != PackageManager.PERMISSION_GRANTED)
+                    list.add(permiso);
 
         }
         return list;
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        String s="";
-        if(requestCode==1)    {
-            for(int i =0; i<permissions.length;i++) {
+        String s = "";
+        if (requestCode == 1) {
+            for (int i = 0; i < permissions.length; i++) {
                 if (grantResults[i] == PackageManager.PERMISSION_GRANTED)
-                    s=s + "OK " + permissions[i] + "\n";
+                    s = s + "OK " + permissions[i] + "\n";
                 else
-                    s=s + "NO  " + permissions[i] + "\n";
+                    s = s + "NO  " + permissions[i] + "\n";
             }
             Toast.makeText(this.getApplicationContext(), s, Toast.LENGTH_LONG).show();
         }
     }
 
-    public void CapturarFoto(View view){
+    public void CapturarFoto(View view) {
+        //BuscarLugar();
+
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        startActivityForResult(intent, RQS_OPEN_DOCUMENT_TREE);
+
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -86,33 +100,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void MostrarDescargas(View view){
+    public void MostrarDescargas(View view) {
 
         Intent intent = new Intent();
         intent.setAction(DownloadManager.ACTION_VIEW_DOWNLOADS);
         startActivity(intent);
 
     }
-    public void BajarDoc(View view){
+
+    public void BajarDoc(View view) {
         String url = "https://www.redalyc.org/pdf/904/90453464013.pdf";
         DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
         request.setDescription("PDF");
         request.setTitle("Pdf");
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                request.allowScanningByMediaScanner();
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-            }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            request.allowScanningByMediaScanner();
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        }
         request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "filedownload.pdf");
         DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
         try {
             manager.enqueue(request);
         } catch (Exception e) {
-            Toast.makeText(this.getApplicationContext(),"Error: "  + e.getMessage(),Toast.LENGTH_LONG).show();
+            Toast.makeText(this.getApplicationContext(), "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
         }
 
     }
 
     private void TomarFoto(View view) {
+        BuscarLugar();
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -121,13 +137,39 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == RQS_OPEN_DOCUMENT_TREE) {
+            Uri uriTree = data.getData();
+            textInfo.append(uriTree.toString() + "\n");
+            textInfo.append("=====================\n");
+            DocumentFile documentFile = DocumentFile.fromTreeUri(this, uriTree);
+            for (DocumentFile file : documentFile.listFiles()) {
+                textInfo.append(file.getName() + "\n");
+
+                if (file.isDirectory()) {
+                    textInfo.append("is a Directory\n");
+                } else {
+                    textInfo.append(file.getType() + "\n");
+                }
+
+                textInfo.append("file.canRead(): " + file.canRead() + "\n");
+                textInfo.append("file.canWrite(): " + file.canWrite() + "\n");
+
+                textInfo.append(file.getUri() + "\n");
+                textInfo.append("---------------------\n");
+            }
+        }
+        /////
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             imageView.setImageBitmap(imageBitmap);
         }
+
+        ///
+
+
+
     }
-
-
 }
